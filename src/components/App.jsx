6da4bfe07d328f17ab9fc6b3dd7800b5a18fb80a
WOWmasterJulia <topazzz@ukr.net>
2023-06-18 12:46:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import { useState } from 'react';
+// import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
@@ -9,18 +10,29 @@ import css from './App.module.css'
 
 const PER_PAGE = 12;
 
-export class App extends Component {
-  state = {
-    searchWord: '',
-    images: [],
-    showModal: false,
-    showMoreBtn: false,
-    bigImage: '',
-    page: 1,
-    error: null,
-    isLoading: false,
-    isEmpty: false,
-  };
+// export class App extends Component {
+//   state = {
+//     searchWord: '',
+//     images: [],
+//     showModal: false,
+//     showMoreBtn: false,
+//     bigImage: '',
+//     page: 1,
+//     error: null,
+//     isLoading: false,
+//     isEmpty: false,
+//   };
+export default function App() {
+  const [searchWord, setSearchWord] = useState('')
+  const [images, setImages] = useState([])
+  const [showModal, setShowModal] = useState(false)
+  const [showMoreBtn, setShowMoreBtn] = useState(false)
+  const [bigImage, setBigImage] = useState('')
+  const [page, setPage] = useState(1)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(false)
+
 
   async componentDidUpdate(_prevProps, prevState) {
     const { searchWord, page } = this.state;
@@ -54,7 +66,7 @@ export class App extends Component {
     }
   }
 
-  onSubmit = evt => {
+  const onSubmit = evt => {
     evt.preventDefault();
     const sw = evt.target.elements[1].value;
     // console.log(sw);
@@ -71,25 +83,24 @@ export class App extends Component {
     }
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const onLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
-  onImageClick = evt => {
-    this.setState({ showModal: true });
-    this.setState({ bigImage: evt.target.dataset.bigimg });
+  const onImageClick = evt => {
+    setImages({ showModal: true });
+    setImages({ bigImage: evt.target.dataset.bigimg });
   };
-  onCloseModal = () => {
-    this.setState({ showModal: false });
-  };
+  const onCloseModal = () => setShowModal( false );
+  
 
-  render() {
-    const { showModal, showMoreBtn, isLoading, isEmpty, bigImage, images } =
-      this.state;
+  // render() {
+  //   const { showModal, showMoreBtn, isLoading, isEmpty, bigImage, images } =
+  //     this.state;
     // console.log(images);
     return (
       <div className={css.App}>
-        <Searchbar myonSubmit={this.onSubmit} />
-        <ImageGallery images={images} onImageClick={this.onImageClick} />
+        <Searchbar myonSubmit={onSubmit} />
+        <ImageGallery images={images} onImageClick={onImageClick} />
         {isLoading && (
           <ColorRing
             visible={true}
@@ -106,15 +117,126 @@ export class App extends Component {
             Sorry. There are no images ... 😭
           </p>
         )}
-        {showMoreBtn && <Button onLoadMore={this.onLoadMore} />}
+        {showMoreBtn && <Button onLoadMore={onLoadMore} />}
         {showModal && (
-          <Modal onClose={this.onCloseModal} bigImage={bigImage}></Modal>
+          <Modal onClose={onCloseModal} bigImage={bigImage}></Modal>
         )}
       </div>
     );
-  }
+  //}
 }
 
+
+
+
+
+
+// const PER_PAGE = 12;
+
+// export class App extends Component {
+//   state = {
+//     searchWord: '',
+//     images: [],
+//     showModal: false,
+//     showMoreBtn: false,
+//     bigImage: '',
+//     page: 1,
+//     error: null,
+//     isLoading: false,
+//     isEmpty: false,
+//   };
+//   async componentDidUpdate(_prevProps, prevState) {
+//     const { searchWord, page } = this.state;
+    
+//     if (prevState.searchWord !== searchWord || prevState.page !== page) {
+//       try {
+//         this.setState({ isLoading: true, error: null });
+        
+//         const resp = await getImagesAPI(searchWord, page);
+//         // console.log("cdu", resp);
+//         if (!resp.length) {
+//           this.setState({
+//             isEmpty: true,
+//             isLoading: false,
+//             showMoreBtn: false,
+//           });
+//           return;
+//         }
+//         const tmp = this.state.images.concat(resp);
+//         // console.log(resp);
+//         this.setState({
+//           images: tmp,
+//           showMoreBtn: resp.length >= PER_PAGE,
+//         });
+//       } catch (error) {
+//         console.log(error.message);
+//         this.setState({ error: error.message });
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+//     }
+//   }
+
+//   onSubmit = evt => {
+//     evt.preventDefault();
+//     const sw = evt.target.elements[1].value;
+//     // console.log(sw);
+//     if (sw !== '') {
+//       this.setState({
+//         searchWord: sw,
+//         page: 1,
+//         images: [],
+//         showModal: false,
+//         bigImage: '',
+//         isLoading: false,
+//         isEmpty: false,
+//       });
+//     }
+//   };
+
+//   onLoadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+//   onImageClick = evt => {
+//     this.setState({ showModal: true });
+//     this.setState({ bigImage: evt.target.dataset.bigimg });
+//   };
+//   onCloseModal = () => {
+//     this.setState({ showModal: false });
+//   };
+
+//   render() {
+//     const { showModal, showMoreBtn, isLoading, isEmpty, bigImage, images } =
+//       this.state;
+//     // console.log(images);
+//     return (
+//       <div className={css.App}>
+//         <Searchbar myonSubmit={this.onSubmit} />
+//         <ImageGallery images={images} onImageClick={this.onImageClick} />
+//         {isLoading && (
+//           <ColorRing
+//             visible={true}
+//             height="80"
+//             width="80"
+//             ariaLabel="blocks-loading"
+//             wrapperStyle={{}}
+//             wrapperClass={css.wrapperClass}
+//             colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+//           />
+//         )}
+//         {isEmpty && (
+//           <p className={css.messageIsEmpty}>
+//             Sorry. There are no images ... 😭
+//           </p>
+//         )}
+//         {showMoreBtn && <Button onLoadMore={this.onLoadMore} />}
+//         {showModal && (
+//           <Modal onClose={this.onCloseModal} bigImage={bigImage}></Modal>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 
 
